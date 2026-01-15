c1dbbaae17734199a40a52d3748de5b59077644d
navsingh3211 <navsingh3211@gmail.com>
2026-01-15 14:33:32 +0530

    config server managerment

---
+++ src/main/java/com/ecommerce/ecomconfigserver/EcomconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomconfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecomconfigserver/EcomconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
